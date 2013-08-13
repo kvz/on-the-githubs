d1b1a3ff5a6aa85b9de4c3f3ab3632cf658cc3b2
Kevin van Zonneveld <kevin@vanzonneveld.net>
2013-08-13 16:07:05 +0200

    Server ./build vs unbuilt ./demo.html

--- demo-server.js
+++ demo-server.js
@@ -4,14 +4,14 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function(req, res) {
   if (req.url === '/') {
-    res.writeHead(301, {'Location': '/demo.html'});
+    res.writeHead(301, {'Location': '/index.html'});
     res.end();
     return;
   }
 
   send(req, req.url)
-    .root(__dirname)
+    .root(__dirname + '/build')
     .pipe(res);
 }).listen(port);
 
-console.log('Demo running at http://127.0.0.1:' + port + '/');
+console.log('Serving ./build at http://127.0.0.1:' + port + '/');
